01e19d5813535a9bb76401f2e6afa041e3e20525
Viktoriya Lozko <v.lozko@soft-industry.com>
2018-02-28 11:44:26 +0200

    Update CartPage.java

--- src/test/java/web_eye_care/pages/CartPage.java
+++ src/test/java/web_eye_care/pages/CartPage.java
@@ -3,10 +3,12 @@ package web_eye_care.pages;
 import io.qameta.allure.Step;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.support.FindBy;
 import web_eye_care.base_classes.BasePage;
 
+import java.util.List;
+
 public class CartPage extends BasePage{
+
 /* TODO: повыносить дублируемый код в универсальные методы, касается всего проекта
    привести подпись аллюр шагов к единому формату например : "Enter email", "Check price" , "Select item from the list" итп. ввести, сделать проверить и тп, также как при ручном описании бага
    написать оббертки под базовые действия управления элементами в классе BasePage.
@@ -18,9 +20,7 @@ public class CartPage extends BasePage{
     private static By priceLocator = By.xpath("//td[@class='item-each']/span");
     private static By subtotalLocator = By.xpath("//td[@class='item-subtotal align-center']/span");
     private static By totalLocator = By.xpath("//tfoot//span[@class='txt-color shopping-total']");
-
     private static By quantityLocator = By.xpath("//td[@class='item-qty']//option[@value='2']");
-
     private static By proceedToCheckoutLocator = By.xpath("//td[@class='cart-right']/button");
 
     private static By returningCustomerFormLocator = By.xpath("//form[@class='white-form shopping-cart-login']");
@@ -28,11 +28,11 @@ public class CartPage extends BasePage{
     private static By passwordLocator = By.xpath("//input[@class='login-password']");
     private static By loginAndCheckoutButtonLocator = By.xpath("//button[@class='btn m-check m-green login-checkout']");
 
+    private static By cartListLocator = By.xpath("//table[@class='cart-table']/tbody/tr");
 
     private static float price;
     private static float subtotal;
     private static float total;
-
     private static int quantity;
 
     private CartPage (){
@@ -51,58 +51,57 @@ public class CartPage extends BasePage{
 
     @Step("Proceed to checkout")
     public static void proceedToCheckout (){
-        WebElement product = driver.findElement(proceedToCheckoutLocator);
-        product.click();
+        clickOnElement(proceedToCheckoutLocator, "Proceed to checkout button does not clickable");
     }
 
     @Step("Remember product price from Cart page")
     public static void setPrice () {
-        WebElement productPrice = driver.findElement(priceLocator);
+        WebElement productPrice = findElementByLocator(priceLocator);
         String strPrice = productPrice.getText().trim();
         strPrice = strPrice.substring(1);
         price = Float.valueOf(strPrice);
     }
 
     @Step("Get product price from Cart page")
-    public static Float getPrice() {
+    public static float getPrice() {
         return price;
     }
 
     @Step("Remember total price from Cart page")
     public static void setTotalPrice () {
-        WebElement totalProductPrice = driver.findElement(totalLocator);
+        WebElement totalProductPrice = findElementByLocator(totalLocator);
         String strPrice = totalProductPrice.getText().trim();
         strPrice = strPrice.substring(1);
         total = Float.valueOf(strPrice);
     }
 
     @Step("Get total price from Cart page")
-    public static Float getTotalPrice() {
+    public static float getTotalPrice() {
         return total;
     }
 
     @Step("Remember subtotal price from Cart page")
     public static void setSubtotalPrice () {
-        WebElement subtotalProductPrice = driver.findElement(subtotalLocator);
+        WebElement subtotalProductPrice = findElementByLocator(subtotalLocator);
         String strPrice = subtotalProductPrice.getText().trim();
         strPrice = strPrice.substring(1);
         subtotal = Float.valueOf(strPrice);
     }
 
     @Step("Get subtotal price from Cart page")
-    public static Float getSubtotalPrice() {
+    public static float getSubtotalPrice() {
         return subtotal;
     }
 
-    @Step("Remember subtotal price from Cart page")
+    @Step("Remember product quantity from Cart page")
     public static void setQuantity() {
-        WebElement productQuantity = driver.findElement(quantityLocator);
+        WebElement productQuantity = findElementByLocator(quantityLocator);
         String strQuantity = productQuantity.getText().trim();
         quantity = Integer.valueOf(strQuantity);
     }
 
     @Step("Get subtotal price from Cart page")
-    public static Integer getQuantity() {
+    public static int getQuantity() {
         return quantity;
     }
 
@@ -149,28 +148,48 @@ public class CartPage extends BasePage{
 
     @Step("Filling returning customer form and sending it in order to login and proceed to checkout")
     public static void loginAndCheckout(String email, String password){
-        fillEmailField(email, emailLocator);
-        fillPasswordField(password, passwordLocator);
+        fillEmailField(email);
+        fillPasswordField(password);
         sendReturningCustomerForm();
     }
 
     @Step("Fill email field with" + "{0}")
-    private static void fillEmailField(String email, By locator){
-        emailField = driver.findElement(locator);
-        emailField.clear();
-        emailField.sendKeys(email);
+    private static void fillEmailField(String email){
+        fillElementWithData(emailLocator, email);
     }
 
     @Step("Fill password field with" + "{0}")
-    private static void fillPasswordField(String password, By locator){
-        passwordField = driver.findElement(locator);
-        passwordField.clear();
-        passwordField.sendKeys(password);
+    private static void fillPasswordField(String password){
+        fillElementWithData(passwordLocator,password);
     }
 
     @Step("Confirm login and checkout on returning customer form")
     private static void sendReturningCustomerForm(){
-        loginAndCheckoutButton = driver.findElement(loginAndCheckoutButtonLocator);
-        loginAndCheckoutButton.click();
+        clickOnElement(loginAndCheckoutButtonLocator, "Login and checkout button does not clickable");
     }
+
+    //--------------------------------------------- clean the cart
+
+    public static void goToTheCart(String url){
+        driver.navigate().to(url);
+    }
+
+    public static void cleanTheCart(){
+        tryToWaitForVisibilityOfElementLocated(wait, shoppingCartFormLocator,"Product form does not visible");
+        tryToWaitForElementToBeClickable(wait, proceedToCheckoutLocator, "Proceed to checkout button does not clickable");
+
+        List<WebElement> emailList = driver.findElements(cartListLocator);
+        int listSize = emailList.size();
+
+        if (listSize == 0){
+            return;
+        }
+
+        for (int i = listSize; i > 0; i--){
+            By locator = By.xpath("//table[@class='cart-table']/tbody/tr[" + i + "]/td[@class='item-remove']");
+            moveToElementAndClickOnIt(locator,"Remove item " + i + "does not present",
+                    "Remove item " + i + "does not clickable");
+        }
+    }
+
 }
