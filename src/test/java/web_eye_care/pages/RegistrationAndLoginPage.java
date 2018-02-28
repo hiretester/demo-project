e6f0234c037483c4cb2a3fa8e460f84856b6db70
Viktoriya Lozko <v.lozko@soft-industry.com>
2018-02-28 11:49:12 +0200

    Update RegistrationAndLoginPage.java

--- src/test/java/web_eye_care/pages/RegistrationAndLoginPage.java
+++ src/test/java/web_eye_care/pages/RegistrationAndLoginPage.java
@@ -2,56 +2,82 @@ package web_eye_care.pages;
 
 import io.qameta.allure.Step;
 import org.openqa.selenium.By;
-import org.openqa.selenium.WebElement;
 import web_eye_care.base_classes.BasePage;
 
-public class ProductPage extends BasePage{
+public class RegistrationAndLoginPage extends BasePage{
 
-    private static By productFormLocator = By.xpath("//form[@name='MainForm']");
-    private static By priceLocator = By.xpath("//div[@class='product-price']//span");
-    private static By addToCartButtonLocator = By.xpath("//div[@class='add-to-cart']/button");
+    private static By registrationFormLocator = By.xpath("//form[@class='login-box m-new']");
+    private static By loginFormLocator = By.xpath("//form[@class='login-box m-login']");
+    private static By emailFieldLocator = By.xpath("//form[@class='login-box m-new']//input[@name='Email']");
+    private static By emailFieldForLoginLocator = By.xpath("//form[@class='login-box m-login']//input[@name='email']");
+    private static By passwordFieldLocator = By.xpath("//form[@class='login-box m-new']//input[@name='password']");
+    private static By passwordFieldForLoginLocator = By.xpath("//form[@class='login-box m-login']//input[@name='password']");
+    private static By repeatPasswordFieldLocator = By.xpath("//form[@class='login-box m-new']//input[@name='passwordagain']");
+    private static By continueButtonLocator = By.xpath("//form[@class='login-box m-new']//button");
+    private static By signInButtonLocator = By.xpath("//form[@class='login-box m-login']//button");
 
-    private static float price;
-
-    private ProductPage (){
+    private RegistrationAndLoginPage(){
     }
 
-    @Step("Checking if product page is loaded")
-    public static boolean isProductPageOpened(String url){
-        boolean isProductFormVisible = tryToWaitForVisibilityOfElementLocated(wait, productFormLocator,"Product form does not visible");
+    @Step("Checking if registration and login page is loaded")
+    public static boolean isRegistrationPageOpened(String url){
+        boolean isRegistrationFormVisible = tryToWaitForVisibilityOfElementLocated(wait, registrationFormLocator,"Registration form does not visible");
 
-        if (!isProductFormVisible){
+        if (!isRegistrationFormVisible){
             return false;
         }
 
         return driver.getCurrentUrl().equals(url);
     }
 
-    @Step("Add product to cart")
-    public static void addToCart (){
-        clickOnElement(addToCartButtonLocator, "Add to cart button does not clickable");
-    }
+    @Step("Checking if registration and login page is loaded")
+    public static boolean isLoginPageOpened(String url){
+        boolean isLoginFormVisible = tryToWaitForVisibilityOfElementLocated(wait, loginFormLocator,"Login form does not visible");
 
-    @Step("Remember product price from Product category page")
-    public static void setPrice () {
-        WebElement productPrice = driver.findElement(priceLocator);
-        String strPrice = productPrice.getText().trim();
-        price = Float.valueOf(strPrice);
-    }
-
-    @Step("Get product price from Product category page")
-    public static float getPrice() {
-        return price;
-    }
-
-    @Step("Checking if price equals to the price from product category page")
-    public static boolean isPriceEqualsToPriceFromProductCategoryPage(){
-        boolean isEqual = false;
-
-        if(price == ProductCategoryPage.getPrice()){
-            isEqual = true;
+        if (!isLoginFormVisible){
+            return false;
         }
 
-        return isEqual;
+        return driver.getCurrentUrl().equals(url);
+    }
+
+    @Step("Filling registration form and sending it in order to register a new user")
+    public static void registerNewUser(String email, String password){
+        fillEmailField(email, emailFieldLocator);
+        fillPasswordField(password, passwordFieldLocator);
+        fillRepeatPasswordField(password, repeatPasswordFieldLocator);
+        sendRegistrationForm();
+    }
+
+    @Step("Filling returning customer form and sending it in order to sign in")
+    public static void signIn(String email, String password){
+        fillEmailField(email, emailFieldForLoginLocator);
+        fillPasswordField(password, passwordFieldForLoginLocator);
+        sendLoginForm();
+    }
+
+    @Step("Fill email field with" + "{0}")
+    private static void fillEmailField(String email, By locator){
+        fillElementWithData(locator, email);
+    }
+
+    @Step("Fill password field with" + "{0}")
+    private static void fillPasswordField(String password, By locator){
+        fillElementWithData(locator, password);fillElementWithData(locator, password);
+    }
+
+    @Step("Fill repeat password field with" + "{0}")
+    private static void fillRepeatPasswordField(String password, By locator){
+        fillElementWithData(locator, password);
+    }
+
+    @Step("Confirm user registration on registration form")
+    private static void sendRegistrationForm(){
+        clickOnElement(continueButtonLocator, "Continue button on registration form does not clickable");
+    }
+
+    @Step("Sign In on login form")
+    private static void sendLoginForm(){
+        clickOnElement(signInButtonLocator, "Sign In button on login form does not clickable");
     }
 }
